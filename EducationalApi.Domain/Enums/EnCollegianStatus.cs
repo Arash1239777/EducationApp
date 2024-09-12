509d3f86cf5edb168787a6339d984032137aff3b
Arash1239777 <arsh123977@gmail.com>
2024-09-12 20:44:14 +0330

    Commited.

--- EducationalApi.Domain/Enums/EnCollegianStatus.cs
+++ EducationalApi.Domain/Enums/EnCollegianStatus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EducationalApi.Domain.Enums
+﻿namespace EducationalApi.Domain.Enums
 {
     public enum EnCollegianStatus
     {
