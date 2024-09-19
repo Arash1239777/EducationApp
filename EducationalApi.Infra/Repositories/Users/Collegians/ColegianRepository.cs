15c19842b86fb1101d3e7a9fd345ed22544ba2ff
Hadi3918 <hadi.sharifzadeh1378@gmail.com>
2024-09-19 20:46:29 +0330

    commit

---
+++ EducationalApi.Infra/Repositories/Users/Collegians/ColegianRepository.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities.Aggrigators.Users.Collegian;
+using EducationalApi.Domain.Entities.Aggrigators.Users.Collegians;
+
+namespace EducationalApi.Infra.Repositories.Users.Collegians;
+internal class ColegianRepository : ICollegianRepository
+{
+    public void Add(Collegian collegian)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Collegian?> FindCollegiansAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Collegian?> FindCollegiansByRoleNumberAsync(string RoleNumber, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Collegian>> GetAll(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
