e6d8fc0739673c33d088ee257d11aebd66338ea2
Arash1239777 <arsh123977@gmail.com>
2024-09-19 20:54:16 +0330

    Commited.

--- EducationalApi.Infra/Repositories/Users/Collegians/ColegianRepository.cs
+++ EducationalApi.Infra/Repositories/Users/Collegians/ColegianRepository.cs
@@ -1,26 +1,31 @@
 ﻿using Domain.Entities.Aggrigators.Users.Collegian;
 using EducationalApi.Domain.Entities.Aggrigators.Users.Collegians;
+using Microsoft.EntityFrameworkCore;
 
 namespace EducationalApi.Infra.Repositories.Users.Collegians;
 internal class ColegianRepository : ICollegianRepository
 {
+    private readonly AppDbContext _context;
+
+    public ColegianRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+
     public void Add(Collegian collegian)
-    {
-        throw new NotImplementedException();
-    }
+        => _context.Collegians.Add(collegian);
 
-    public Task<Collegian?> FindCollegiansAsync(long id)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<Collegian?> FindCollegiansAsync(long id)
+        => await _context.Collegians.FindAsync(id);
 
-    public Task<Collegian?> FindCollegiansByRoleNumberAsync(string RoleNumber, CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<Collegian?> FindCollegiansByRoleNumberAsync(string RoleNumber, CancellationToken cancellationToken)
+        => await _context.Collegians
+            .AsTracking()
+            .FirstOrDefaultAsync(collegian => collegian.RoleNumber.Equals(RoleNumber), cancellationToken);
 
-    public Task<IReadOnlyList<Collegian>> GetAll(CancellationToken cancellationToken)
-    {
-        throw new NotImplementedException();
-    }
+    public async Task<IReadOnlyList<Collegian>> GetAll(CancellationToken cancellationToken)
+        => await _context.Collegians
+            .AsNoTrackingWithIdentityResolution()
+            .ToListAsync(cancellationToken);
 }
